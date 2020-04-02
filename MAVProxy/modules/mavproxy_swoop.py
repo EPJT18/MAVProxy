36f2a0afe55fd5bd003688886a8281e9a977b7a6
Josh <joshua.tepper@outlook.com>
2020-04-02 15:24:10 +1100

    Flaggin check module

--- MAVProxy/modules/mavproxy_swoop.py
+++ MAVProxy/modules/mavproxy_swoop.py
@@ -5,6 +5,7 @@ import time, os
 from MAVProxy.modules.lib import mp_module
 from pymavlink import mavutil
 import sys, traceback
+from datetime import datetime
 
 class SwoopModule(mp_module.MPModule):
     def __init__(self, mpstate):
@@ -14,10 +15,78 @@ class SwoopModule(mp_module.MPModule):
     def mavlink_packet(self, m):
         'handle a mavlink packet'''
 
-        if m.get_type() == 'SWOOP_FLAGS':
-           	print ('Got a Flagging Packet')
-	        print (str(m.armingFlags1))
-
+        if m.get_type() == 'SWOOP_INFLIGHT_FLAGS_INSTANT':
+            if(m.inflightFlags>0):
+                now = datetime.now()
+                current_time = now.strftime("%H:%M:%S")
+                print("Current Time =", current_time)
+                print( 'Inflight flags  ' + str(m.inflightFlags))
+            if(m.maximumIntensity>0):
+                print('Max Intensity  ' + str(m.maximumIntensity))
+            if(m.hoverAssistIntensity>0):
+                print('Hover Asssit Intensity ' + str(m.hoverAssistIntensity))
+            if(m.emergencyLandIntensity>0):
+                print('Emergency Land Intensity ' + str(m.emergencyLandIntensity))
+            if(m.gpsIntensity>0):
+                print('GPS Intensity ' + str(m.gpsIntensity))
+            if(m.vibrationIntensity>0):
+                print('Vibration Intensity ' + str(m.vibrationIntensity))
+            if(m.hoverMotorIntensity>0):
+                print('Hover Motor Intensity ' + str(m.hoverMotorIntensity))
+            if(m.forwardMotorIntensity>0):
+                print('Forward Motor Intensity ' + str(m.forwardMotorIntensity))
+            if(m.lidarIntensity>0):
+                print('Lidar Intensity ' + str(m.lidarIntensity))
+            if(m.hoverBatteryIntensity>0):
+                print('Hover Battery Intensity ' + str(m.hoverBatteryIntensity))
+            if(m.forwardBatteryIntensity>0):
+                print('Hover Battery Intensity ' + str(m.forwardBatteryIntensity))
+            if(m.altitudeIntensity >0 ):
+                print('Altitude Intensity ' + str(m.altitudeIntensity))
+            if(m.windIntensity >0 ):
+                print('Wind Intensity ' + str(m.windIntensity))
+            if(m.hoverAttitudeIntensity >0 ):
+                print('Hover Attitude Intensity ' + str(m.hoverAttitudeIntensity))
+            if(m.landingIntensity >0 ):
+                print('Landing Intensity ' + str(m.landingIntensity))
+            if(m.aerodynamicIntensity >0 ):
+                print('Aerodynamic Intensity ' + str(m.aerodynamicIntensity))
+            if(m.airspeedIntensity >0 ):
+                print('Airspeed Intensity ' + str(m.airspeedIntensity))
+            if(m.servoIntensity >0 ):
+                print('Servo Intensity ' + str(m.servoIntensity))
+            if(m.hoverAssistDetail>0):
+                print('Hover Asssit Detail ' + str(m.hoverAssistDetail))
+            if(m.emergencyLandDetail>0):
+                print('Emergency Land Detail' + str(m.emergencyLandDetail))
+            if(m.gpsDetail>0):
+                print('GPS Detail ' + str(m.gpsDetail))
+            if(m.vibrationDetail>0):
+                print('Vibration Detail ' + str(m.vibrationDetail))
+            if(m.hoverMotorDetail>0):
+                print('Hover Motor Detail ' + str(m.hoverMotorDetail))
+            if(m.forwardMotorDetail>0):
+                print('Forward Motor Detail ' + str(m.forwardMotorDetail))
+            if(m.lidarDetail>0):
+                print('Lidar Detail ' + str(m.lidarDetail))
+            if(m.hoverBatteryDetail>0):
+                print('Hover Battery Detail ' + str(m.hoverBatteryDetail))
+            if(m.forwardBatteryDetail>0):
+                print('Hover Battery Detail ' + str(m.forwardBatteryDetail))
+            if(m.altitudeDetail >0 ):
+                print('Altitude Detail ' + str(m.altitudeDetail))
+            if(m.windDetail >0 ):
+                print('Wind Detail ' + str(m.windDetail))
+            if(m.hoverAttitudeDetail >0 ):
+                print('Hover Attitude Detail ' + str(m.hoverAttitudeDetail))
+            if(m.landingDetail >0 ):
+                print('Landing Detail ' + str(m.landingDetail))
+            if(m.aerodynamicDetail >0 ):
+                print('Aerodynamic Detail ' + str(m.aerodynamicDetail))
+            if(m.airspeedDetail >0 ):
+                print('Airspeed Detail ' + str(m.airspeedDetail))
+            if(m.servoDetail >0 ):
+                print('Servo Detail ' + str(m.servoDetail))
             
 
 def init(mpstate):
